ed96c6673c081cbc719ee858a845ebc3871ffe0d
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2026-03-02 18:45:29 +0300

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Manufacturer, Product, Cart, CartItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock_quantity', 'category', 'manufacturer')
+    list_filter = ('category', 'manufacturer')
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 1
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'total_cost')
+    inlines = [CartItemInline]
+
