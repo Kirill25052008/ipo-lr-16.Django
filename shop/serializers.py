6b2153444fc2637bce272d37daae97b0c5180f2d
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2026-04-16 19:20:29 +0300

    Update serializers.py

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,1 +1,34 @@
+from rest_framework import serializers
+from .models import Product, Category, Manufacturer, Cart, CartItem
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manufacturer
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    manufacturer = ManufacturerSerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'cart']
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'items', 'created_at']
